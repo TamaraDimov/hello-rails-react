4a77dc63f0f4310f0a21a89b107ee107505a9599
TamaraDimov <tamemojsoska@gmail.com>
2023-09-19 00:44:57 +0200

    generate random greetings

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,5 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+Message.create(content: "Welcome! How are you?")
+Message.create(content: "Good morning! How can i help you?")
+Message.create(content: "Hi! Feel free to ask any questions.")
+Message.create(content: "Hi there! Nice to meet you!")
+Message.create(content: "Good morning! Have a great day.")
